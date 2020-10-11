34c455f3e1c661d640f34f4945945ba26a26ec1a
rahulkookal <rahulkookal@qburst.com>
2020-10-11 14:34:44 +0530

    update

--- db/index.js
+++ db/index.js
@@ -5,49 +5,11 @@ var MongoClient = require('mongodb').MongoClient,
 //Specify the Amazon DocumentDB cert
 var ca = [fs.readFileSync("./cert/rds-combined-ca-bundle.pem")];
 
-//Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set, 
-//  and specify the read preference as secondary preferred
-// let con;
-// const init = function() {
-//   MongoClient.connect(
-//     'mongodb://rahul:rahulkodoth@docdb-2020-10-10-08-45-24.cluster-c6jhpcbeair9.ap-southeast-2.docdb.amazonaws.com:27017/?ssl=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false', 
-//     { 
-//       sslValidate: true,
-//       sslCA:ca,
-//       useNewUrlParser: true
-//     }
-//     ,
-//     function(err, client) {
-//         if(err)
-//             throw err;
-//             console.log("Connection created")
-//             con =  client
-        // let db = client.db('customer_1')
-        // col = db.collection('users');
-        // col.insertOne({'hello':'Amazon DocumentDB'}, function(err, result){
-        //   console.log("ee")
-        //   console.log(result)
-    //     // })
-    // }
-    // )
-    // .then((client) => con = client);
-
-// }
-// const insertUser = (req, res) => {
-//   let db = con.db('customer_1')
-//       col = db.collection('users');
-//       col.insertOne({'hello':'Amazon DocumentDB'}, function(err, result){
-//         console.log("ee")
-//         console.log(result)
-//       })
-// }
-// module.exports = { init, insertUser }
-
-
-
-
 const PRIMARY = "mongodb://rahul:rahulkodoth@docdb-2020-10-10-08-45-24.c6jhpcbeair9.ap-southeast-2.docdb.amazonaws.com:27017/?ssl=true&retryWrites=false"
 const REPLICA_1 = "mongodb://rahul:rahulkodoth@docdb-2020-10-10-08-45-242.c6jhpcbeair9.ap-southeast-2.docdb.amazonaws.com:27017/?ssl=true&retryWrites=false"
+const REPLICA_2 = "mongodb://rahul:rahulkodoth@docdb-2020-10-10-08-45-242.c6jhpcbeair9.ap-southeast-2.docdb.amazonaws.com:27017/?ssl=true&retryWrites=false"
+const REPLICA_3 = "mongodb://rahul:rahulkodoth@docdb-2020-10-10-08-45-242.c6jhpcbeair9.ap-southeast-2.docdb.amazonaws.com:27017/?ssl=true&retryWrites=false"
+
 
 function connect(url) {
   return MongoClient.connect(url, { 
@@ -58,11 +20,13 @@ function connect(url) {
 }
 
 module.exports = async function() {
-  let databases = await Promise.all([connect(PRIMARY), connect(REPLICA_1)])
+  let databases = await Promise.all([connect(PRIMARY), connect(REPLICA_1), connect(REPLICA_2), connect(REPLICA_3)])
 
   return {
     PRIMARY: databases[0],
-    REPLICA_1: databases[1]
+    REPLICA_1: databases[1], 
+    REPLICA_2: databases[2],
+    REPLICA_3: databases[3]
   }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -28,4 +28,17 @@ router.post('/', function(req, res, next) {
         })
 });
 
+router.get('/:id', function(req, res, next) {
+  let client = global.connections.REPLICA_1
+  console.log(req.params)
+  let db = client.db('customer_1')
+      col = db.collection('users');
+      col.find({ _id: ObjectId("5f82c5d1f80cc754dc9d77ba") }, function(err, result) {        
+        if(err){
+          res.status(500).json(err).end()
+        }
+        res.status(200).json(result).end()
+      })
+});
+
 module.exports = router;
