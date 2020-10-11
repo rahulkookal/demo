54a6b3add8832cc506e8ee0ff5c5f26dd960002a
rahulkookal <rahulkookal@qburst.com>
2020-10-11 13:38:05 +0530

    Update

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-const {insertUser} = require('../db/index')
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({test: "sds"});
 });
 
 router.post('/', function(req, res, next) {
-  insertUser(req, res)
+  let client = global.connections.PRIMARY
+  let db = client.db('customer_1')
+        col = db.collection('users');
+        col.insertOne({'hello':'Amazon DocumentDB'}, function(err, result){
+          console.log("ee")
+        // })
+  res.json({test: "sds"});
 });
 
 module.exports = router;
