6f65c1b5dec9c3c069c0f1c2be1431281c1ff2a4
rahulkookal <rahulkookal@qburst.com>
2020-10-11 15:08:54 +0530

    Update

--- routes/users.js
+++ routes/users.js
@@ -21,18 +21,31 @@ router.post('/', function(req, res, next) {
   let db = client.db('customer_1')
       col = db.collection('users');
         col.insertOne(req.body.user, function(err, result){
-          console.log("ee")
-          console.log(result)
+          console.log(req.body.user)
           if(err){
             res.status(500).json(err).end()
           }
-          res.status(200).json({id: "sds"}).end()
+          res.status(200).json(result).end()
         })
 });
 
 router.get('/:id', function(req, res, next) {
   let client = global.connections.REPLICA_1
   console.log(req.params)
+  let db = client.db('customer_1')
+      col = db.collection('users');
+      col.find({ _id: ObjectId(req.params.id) }).toArray(function(err, result) {        
+        if(err){
+          res.status(500).json(err).end()
+        }
+        res.status(200).json(result).end()
+      });
+});
+
+router.put('/:id', function(req, res, next) {
+  let client = global.connections.REPLICA_1
+  console.log(req.params)
+  console.log(req.body.user)
   let db = client.db('customer_1')
       col = db.collection('users');
       col.find({ _id: ObjectId("5f82c5d1f80cc754dc9d77ba") }).toArray(function(err, result) {        
