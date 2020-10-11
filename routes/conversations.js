e1de810ca578c1da92c8034f2abb43f01dbe6f94
rahulkookal <rahulkookal@qburst.com>
2020-10-11 19:17:52 +0530

    Update

--- routes/conversations.js
+++ routes/conversations.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET conversations listing. */
 router.get('/', function(req, res, next) {
-  res.json({test: "sds"});
+  let client = global.connections.REPLICA_1
+  console.log(req.params)
+  let db = client.db('customer_1')
+      col = db.collection('conversations');
+      col.find({}).toArray(function(err, result) {        
+        if(err){
+          res.status(500).json(err).end()
+        }
+        res.status(200).json(result).end()
+      });
 });
+
+router.post('/', function(req, res, next) {
+  let client = global.connections.PRIMARY
+  let db = client.db('customer_1')
+      col = db.collection('conversations');
+        col.insertOne(req.body.conversation, function(err, result){
+          console.log(req.body.conversation)
+          if(err){
+            res.status(500).json(err).end()
+          }
+          res.status(200).json(result).end()
+        })
+});
+
+router.get('/:id', function(req, res, next) {
+  let client = global.connections.REPLICA_1
+  console.log(req.params)
+  let db = client.db('customer_1')
+      col = db.collection('conversations');
+      col.find({ _id: ObjectId(req.params.id) }).toArray(function(err, result) {        
+        if(err){
+          res.status(500).json(err).end()
+        }
+        res.status(200).json(result).end()
+      });
+});
+
+router.put('/', function(req, res, next) {
+  let client = global.connections.PRIMARY
+  let conversation = req.body.conversation
+  delete user._id;
+  let updateValues = { $set: conversation };
+  let db = client.db('customer_1')
+      col = db.collection('conversations');
+      col.findOneAndUpdate({ _id: ObjectId(req.body.user._id) }, updateValues).then(function(err, result) {        
+        if(err){
+          res.status(500).json(err).end()
+        }
+        res.status(200).json(result).end()
+      });
 
 module.exports = router;
--- routes/visitors.js
+++ routes/visitors.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET visitors listing. */
 router.get('/', function(req, res, next) {
-  res.json({test: "sds"});
+  let client = global.connections.REPLICA_2
+  console.log(req.params)
+  let db = client.db('customer_1')
+      col = db.collection('visitors');
+      col.find({}).toArray(function(err, result) {        
+        if(err){
+          res.status(500).json(err).end()
+        }
+        res.status(200).json(result).end()
+      });
+});
+
+router.post('/', function(req, res, next) {
+  let client = global.connections.PRIMARY
+  let db = client.db('customer_1')
+      col = db.collection('visitors');
+        col.insertOne(req.body.visitor, function(err, result){
+          console.log(req.body.visitor)
+          if(err){
+            res.status(500).json(err).end()
+          }
+          res.status(200).json(result).end()
+        })
+});
+
+router.get('/:id', function(req, res, next) {
+  let client = global.connections.REPLICA_2
+  console.log(req.params)
+  let db = client.db('customer_1')
+      col = db.collection('visitors');
+      col.find({ _id: ObjectId(req.params.id) }).toArray(function(err, result) {        
+        if(err){
+          res.status(500).json(err).end()
+        }
+        res.status(200).json(result).end()
+      });
+});
+
+router.put('/', function(req, res, next) {
+  let client = global.connections.PRIMARY
+  let visitor = req.body.visitor
+  delete visitor._id;
+  let updateValues = { $set: visitor };
+  let db = client.db('customer_1')
+      col = db.collection('visitors');
+      col.findOneAndUpdate({ _id: ObjectId(req.body.visitor._id) }, updateValues).then(function(err, result) {        
+        if(err){
+          res.status(500).json(err).end()
+        }
+        res.status(200).json(result).end()
+      });
 });
 
 module.exports = router;
