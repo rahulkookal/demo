2e71735c4652e88d93231d3143f4d57e4e7ba135
rahulkookal <rahulkookal@qburst.com>
2020-10-11 14:54:00 +0530

    Update

--- routes/users.js
+++ routes/users.js
@@ -35,13 +35,12 @@ router.get('/:id', function(req, res, next) {
   console.log(req.params)
   let db = client.db('customer_1')
       col = db.collection('users');
-      col.find({ _id: ObjectId("5f82c5d1f80cc754dc9d77ba") }, function(err, result) {        
+      col.find({ _id: ObjectId("5f82c5d1f80cc754dc9d77ba") }).toArray(function(err, result) {        
         if(err){
           res.status(500).json(err).end()
         }
-        console.log(result)
         res.status(200).json(result).end()
-      })
+      });
 });
 
 module.exports = router;
