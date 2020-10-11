ac9380064f2792e662f9c30ba89e61b89066543e
rahulkookal <rahulkookal@qburst.com>
2020-10-11 15:20:40 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -42,24 +42,18 @@ router.get('/:id', function(req, res, next) {
       });
 });
 
-router.put('/:id',function(req, res, next){
-  console.log(req.body.user)
+router.put('/', function(req, res, next) {
+  let client = global.connections.REPLICA_1
   console.log(req.params)
-  res.status(200).json({status: 'ok'}).end()
-})
-
-// router.put('/:id', function(req, res, next) {
-//   let client = global.connections.REPLICA_1
-//   console.log(req.params)
-//   console.log(req.body.user)
-//   let db = client.db('customer_1')
-//       col = db.collection('users');
-//       col.find({ _id: ObjectId("5f82c5d1f80cc754dc9d77ba") }).toArray(function(err, result) {        
-//         if(err){
-//           res.status(500).json(err).end()
-//         }
-//         res.status(200).json(result).end()
-//       });
-// });
+  console.log(req.body.user)
+  let db = client.db('customer_1')
+      col = db.collection('users');
+      col.find({ _id: ObjectId("5f82c5d1f80cc754dc9d77ba") }).toArray(function(err, result) {        
+        if(err){
+          res.status(500).json(err).end()
+        }
+        res.status(200).json(result).end()
+      });
+});
 
 module.exports = router;
